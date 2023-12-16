34612de6a21d734857f2d3498ca9d11dd70c5a7b
blekk89 <153469102+blekk89@users.noreply.github.com>
2023-12-16 23:17:22 +0100

    gggg

--- inscription.php
+++ inscription.php
@@ -10,6 +10,13 @@ $prenom= $_REQUEST['prenom'];
 $nom_mere =  $_REQUEST['nom_mere'];
 $nom_pere = $_REQUEST['nom_pere'];
 $adresse = $_REQUEST['addresse'];
+$genre = $_REQUEST['genre'];
+$region = $_REQUEST['region'];
+$departement = $_REQUEST['addresse'];
+$profession = $_REQUEST['profession'];
+$cin = $_REQUEST['cin'];
+$numero_tel = $_REQUEST['numero_tel'];
+
 
 // Create connection
 $conn = new mysqli($servername, 
@@ -20,9 +27,10 @@ if ($conn->connect_error) {
 	die("Connection failed: "
 		. $conn->connect_error);
 }
+$id_citizen = substr($nom, 1, 3) || substr($prenom, 1, 3) || substr($nom_mere, 1, 3) || substr($nom_pere, 1, 3) || substr($genre, 1, 3) || substr($region, 1, 3) || substr($departement, 1, 3) || substr($cin, 1, 10);
 
 $sql = "INSERT INTO citoyen(ID_citizen,Nom,Prenom,Nom_mere,Nom_pere,Addresse,Genre,Region,Departement,Profession,Cin,Numero_Tel) VALUES 
-	('$id_citizen', '$nom', '$prenom','$nom_mere','$nom_pere','$adresse','homme','dakar','guediawaye','it','00123458','33965242')";
+	('$id_citizen', '$nom', '$prenom','$nom_mere','$nom_pere','$adresse','$genre','$region','$departement','$profession','$cin ','$numero_tel')";
 
 if ($conn->query($sql) === TRUE) {
 	echo "record inserted successfully";
