e0333d08781d2d9564ee44dd4eb0baa05232ef00
blekk89 <153469102+blekk89@users.noreply.github.com>
2023-12-16 15:51:00 +0100

    cccc

---
+++ inscription.php
@@ -0,0 +1,47 @@
+<?php
+
+$servername = "localhost";
+$username = "admin";
+$password = "";
+$dbname = "anagrafica_sn";
+$nom =  $_REQUEST['nom'];
+$prenom= $_REQUEST['prenom'];
+$first_name =  $_REQUEST['nom_mere'];
+$last_name = $_REQUEST['nom_pere'];
+
+// Create connection
+$conn = new mysqli($servername, 
+	$username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+	die("Connection failed: "
+		. $conn->connect_error);
+}
+
+$sqlquery = "INSERT INTO Citoyen VALUES 
+	('$nom', 
+            '$prenom','$nom_mere','$nom_pere','$adresse')";
+
+if ($conn->query($sql) === TRUE) {
+	echo "record inserted successfully";
+} else {
+	echo "Error: " . $sql . "<br>" . $conn->error;
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+ 
+    // collect value of input field
+    $data = $_REQUEST['val1'];
+ 
+    if (empty($data)) {
+        echo "data is empty";
+    } else {
+        echo $data;
+    }
+}
+
+ 
+// Closing the connection.
+$conn->close();
+ 
+?>
